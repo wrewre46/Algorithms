cc54d62cc2a89b72d40ff56910f05353dbd8a70c
jinwoo <wrewre46@naver.com>
2021-11-18 14:44:26 +0900

    BOJ

--- BOJ/입출력/No_10869.java
+++ BOJ/입출력/No_10869.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-public class Main{
+public class No_10869{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
