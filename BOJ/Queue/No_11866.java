67a12336088d1e6ed595308f6e33c46827661dda
wrewre46 <wrewre46@naver.com>
2022-01-08 23:07:40 +0900

    BOJ

---
+++ BOJ/Queue/No_11866.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class No_11866{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int N = Integer.valueOf(st.nextToken());
+        int K = Integer.valueOf(st.nextToken());
+        Queue<Integer> que= new LinkedList<>();
+        for(int i = 1 ;i<=N;i++){
+            que.offer(i);
+        }
+        bw.write("<");
+        while(que.size()!=1){
+            for(int i=1; i<K ;i++){
+                que.offer(que.peek());
+                que.poll();
+            }
+            bw.write(String.valueOf(que.peek())+", ");
+            que.poll();
+        }
+        bw.write(String.valueOf(que.peek())+">");
+        bw.flush();
+        bw.close();
+    
+    }
+    
+}
+
---
+++ BOJ/Queue/No_2164.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class No_2164 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int N = Integer.parseInt(str);
+        Queue<Integer> que= new LinkedList<>();
+        for(int i = 1 ;i<=N;i++){
+            que.offer(i);
+        }
+        while(que.size()!=1){
+            que.poll();
+            que.offer(que.peek());
+            que.poll();
+        }
+        bw.write(String.valueOf(que.peek()));
+        bw.flush();
+        bw.close();
+    
+    }
+    
+}
+
